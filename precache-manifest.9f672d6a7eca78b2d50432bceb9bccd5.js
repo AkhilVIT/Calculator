31577ef5e4b3a7a54af4b633f7c88466fb47f064
AkhilVIT <makhilakhi01@gmail.com>
2020-08-09 15:13:43 +0530

    Updates

---
+++ precache-manifest.9f672d6a7eca78b2d50432bceb9bccd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44380359f354cadc3e125041ee3f57c",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "60c8d642b6e1630758e6",
+    "url": "/Calculator/static/css/main.9e4b2e71.chunk.css"
+  },
+  {
+    "revision": "fc4efc6db883aef968d5",
+    "url": "/Calculator/static/js/2.ffb6248d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.ffb6248d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c8d642b6e1630758e6",
+    "url": "/Calculator/static/js/main.0b421ab9.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
